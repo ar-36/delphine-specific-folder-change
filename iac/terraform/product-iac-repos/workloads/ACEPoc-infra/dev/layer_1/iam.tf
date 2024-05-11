1d32262c4555414b5063666a54ae9a929fbae92c
ar-36 <ammarzlve@gmail.com>
2024-05-12 00:54:56 +0500

    erewrw

--- iac/terraform/product-iac-repos/workloads/ACEPoc-infra/dev/layer_1/iam.tf
+++ iac/terraform/product-iac-repos/workloads/ACEPoc-infra/dev/layer_1/iam.tf
@@ -10,6 +10,7 @@
 # The contents of this file are automatically generated, and will be overwritten!
 ####################################################################################################
 
+
 data "aws_iam_policy_document" "gha" {
   statement {
     actions = ["sts:AssumeRoleWithWebIdentity"]
